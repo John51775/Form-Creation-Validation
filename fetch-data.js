b53e8dea7e63592c5dafb4918d44b131f6585868
John Ngugi <ngaruiyajohn86@gmail.com>
2025-11-29 17:29:18 +0300

    Fetching Data from an API and Displaying It

---
+++ fetch-data.js
@@ -0,0 +1,30 @@
+ 
+ document.addEventListener('DOMContentloaded', () => {fetchUserData();});
+ 
+ async function fetchUserData(){
+
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    const dataContainer = document.getElementById('api-data');
+
+    try { const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = '';
+        const userList = document.createElement('ul');
+
+        users.forEach(user => {
+            const li = document.createElement('li');
+            li.textContent = user.name;
+            userList.appendChild(li);
+        });
+
+        dataContainer.appendChild(userList);
+
+
+    } catch (error){
+
+            console.error ('Error fetching user data:', error);
+             dataContainer.innerHTML = '';
+            dataContainer.textContent = 'Failed to load user data.';
+    }
+}
